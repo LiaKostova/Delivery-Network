01336260c7de8107d812a35e54cc5ea5787d7643
LiaKostova <123201316+LiaKostova@users.noreply.github.com>
2024-02-23 17:08:43 +0200

    Chart that shows the charging logic
    A chart that shows the logic behind checking whether a drone needs to be loaded and, if so, how long it will take to load and execute that particular order.

---
+++ utilsForDelivering/calculatingNeededTimeForTheDeliver.js
@@ -0,0 +1,21 @@
+function calculatingNeededTimeForchargeandDeliver(dronPower, distance, powerReserv ){
+    
+    let chargedPowerPerMin = (Number(dronPower))/20;
+    //neededDistance is actual equal to power;
+
+    let powerWeMiss = distance - powerReserv;
+
+    let alreadyChargedPower = 0;
+    let howManyMinutesChargingTakses
+
+    while(alreadyChargedPower<powerWeMiss){
+        alreadyChargedPower += chargedPowerPerMin;
+        howManyMinutesChargingTakses++;
+    }
+
+    let totalMinutesToFulFillTheOrder = distance + 5 + howManyMinutesChargingTakses;
+
+    return totalMinutesToFulFillTheOrder;
+}
+
+module.exports = calculatingNeededTimeForchargeandDeliver;
