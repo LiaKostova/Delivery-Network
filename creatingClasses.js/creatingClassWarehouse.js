2340889293fe13b1aec132ae84609caf341b3179
LiaKostova <123201316+LiaKostova@users.noreply.github.com>
2024-02-23 22:28:12 +0200

    Some Class Warehouse Changes

--- creatingClasses.js/creatingClassWarehouse.js
+++ creatingClasses.js/creatingClassWarehouse.js
@@ -1,13 +1,11 @@
 class Warehouse{
-    constructor(allOrdersWeCanDeliver, totalDistance, timeNeededForAllOrders, remainingPowerOfTheDay, remainingMinutesOfTheDay, undeliverableOrders, temporarilyPossibleOrders, temporarilyPossibleOrdersDistnace, originaFullCapacityPower ){
+    constructor(allOrdersWeCanDeliver, totalDistance, timeNeededForAllOrders, remainingPowerOfTheDay, remainingMinutesOfTheDay, undeliverableOrders,originaFullCapacityPower){
         this.allOrdersWeCanDeliver = allOrdersWeCanDeliver;
         this.totalDistance= totalDistance;
         this.timeNeededForAllOrders = timeNeededForAllOrders;
         this.remainingPowerOfTheDay = remainingPowerOfTheDay;
         this.remainingMinutesOfTheDay = remainingMinutesOfTheDay;
         this.undeliverableOrders = undeliverableOrders;
-        this.temporarilyPossibleOrders = temporarilyPossibleOrders;
-        this.temporarilyPossibleOrdersDistnace = temporarilyPossibleOrdersDistnace;
         this.originaFullCapacityPower = originaFullCapacityPower;
     }
 }
