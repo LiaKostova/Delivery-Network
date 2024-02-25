e8b63fd24eb2efc78d2af7c2696e0bb1b2759166
LiaKostova <123201316+LiaKostova@users.noreply.github.com>
2024-02-25 14:30:17 +0200

    correct output
    Adding the correct output for a warehouse where there were no orders.

--- project.js
+++ project.js
@@ -116,8 +116,17 @@ let dailyWorkInfoForAllWaehouses = dailyWorkInfo.pop();
 
 let textForEachWh = '';
 for(let wh of dailyWorkInfo){
-   let textInfoForThisWh =  `The warehouse '${wh.whName}' used ${wh.usedDrones} drones to deliver ${wh.numOfOrders} orders. Its average delivery time is ${wh.averageTimeForAnOrderForThisWh} minutes.` + '\n'
-    textForEachWh += textInfoForThisWh;
+    
+    if(wh.numOfOrders == 0){
+        let textForZeroOrders = `The warehouse '${wh.whName}' had no orders for delivery.` + '\n';
+        textForEachWh += textForZeroOrders;
+
+    }else{
+        let textInfoForThisWh =  `The warehouse '${wh.whName}' used ${wh.usedDrones} drones to deliver ${wh.numOfOrders} orders. Its average delivery time is ${wh.averageTimeForAnOrderForThisWh} minutes.` + '\n'
+        textForEachWh += textInfoForThisWh;
+    }
+    
+    
 }
 
 let finalText = `Total Used Drones for today: ${dailyWorkInfoForAllWaehouses.totalUsedDrones}` + '\n' + `Average time for all orders: ${dailyWorkInfoForAllWaehouses.totalAverageTime} minutes`
